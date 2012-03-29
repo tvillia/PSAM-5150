6126f85de34775efd22a019e18d47427b1733746
Terese Villia <villt583@newschool.edu>
2012-03-29 18:16:33 -0400

    url admin…

--- psam5150/urls.py
+++ psam5150/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', 'signupform.views.home', name='home'),
     url(r'^signup/', include('signupform.urls')),
     url(r'^gameshow/', include('gameshow.urls')),
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
