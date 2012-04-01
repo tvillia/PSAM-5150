a9420d64ddc147fd03d7db4bc19d6440afe0f990
Terese Villia <villt583@newschool.edu>
2012-04-01 11:01:14 -0400

    …

--- psam5150/contact/models.py
+++ psam5150/contact/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-class Contact(models.Model):# Create your models here.
+class Contact(models.Model):
+
+	name = models.CharField(max_length=100)
+	address = models.charField(max_length_length=200)
+	# Create your models here.
